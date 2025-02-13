14d1ac661ba2b7b0bd61643c95587c3abfa3feb0
Vivek Kumar <vivek.kumar@lpu.in>
2025-02-13 23:15:58 +0530

    Created frontend of my chess game

---
+++ Render/main.js
@@ -0,0 +1,15 @@
+const ROOT_DIV = document.getElementById("root");
+
+export function initGameRender(data){
+    data.forEach(element => {
+        const rowEl = document.createElement("div");
+        element.forEach((square) => {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add(square.color,"square");
+            rowEl.appendChild(squareDiv);
+        });
+        
+        rowEl.classList.add("squareRow");
+        ROOT_DIV.appendChild(rowEl);
+    });
+};
