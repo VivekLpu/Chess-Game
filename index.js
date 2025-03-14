531df11ae902bbe1a0179a6587cc31e13399b46b
Vivek Kumar <118115653+VivekLpu@users.noreply.github.com>
2025-03-14 23:38:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { initGame } from "./data/data.js";
 import { initGameRender } from "./Render/main.js";
+import { globalEvent } from "./Events/global.js";
 
-<<<<<<< HEAD
 //will be useful till game ends
 const globalState = initGame();
 
 initGameRender(globalState);
-=======
-initGameRender(initGame());
->>>>>>> 14d1ac661ba2b7b0bd61643c95587c3abfa3feb0
+globalEvent();
+export {globalState};
