67ce7355619e428ad5afc742a313c3d35780b437
Vivek Kumar <118115653+VivekLpu@users.noreply.github.com>
2025-03-14 23:34:55 +0530

    Update pieces.js

--- data/pieces.js
+++ data/pieces.js
@@ -1,52 +1,98 @@
 
 //black pieces
-export function blackPawn(current_position) {
-    return {
+export function blackPawn(current_position){
+    return{
         current_position,
         img : "assets/images/pieces/black/pawn.png",
+        piece_name : "BLACK_PAWN",
     };
 }
 
+export function blackRook(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/black/rook.png",
+        piece_name : "BLACK_ROOK",
+    };
+}
+
+export function blackKnight(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/black/knight.png",
+        piece_name : "BLACK_KNIGHT",
+    };
+}
+
+export function blackBishop(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/black/bishop.png",
+        piece_name : "BLACK_BISHOP",
+    };
+}
+
+export function blackQueen(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/black/queen.png",
+        piece_name : "BLACK_QUEEN",
+    };
+}
+
+export function blackKing(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/black/king.png",
+        piece_name : "BLACK_KING",
+    };
+}
 //white pieces
-export function whitePawn(current_position) {
-    return {
+export function whitePawn(current_position){
+    return{
         current_position,
         img : "assets/images/pieces/white/pawn.png",
+        piece_name : "WHITE_PAWN",
     };
 }
 
-export function whiteRook(cuurent_position){
-    return {
-        cuurent_position,
+export function whiteRook(current_position){
+    return{
+        current_position,
         img : "assets/images/pieces/white/rook.png",
+        piece_name : "WHITE_ROOK",
     };
 }
 
-export function whiteRook(cuurent_position){
-    return {
-        cuurent_position,
-        img : "assets/images/pieces/white/rook.png",
+export function whiteKnight(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/white/knight.png",
+        piece_name : "WHITE_KNIGHT",
     };
 }
 
-export function whiteRook(cuurent_position){
-    return {
-        cuurent_position,
-        img : "assets/images/pieces/white/rook.png",
+export function whiteBishop(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/white/bishop.png",
+        piece_name : "WHITE_BISHOP",
     };
 }
 
-export function whiteRook(cuurent_position){
-    return {
-        cuurent_position,
-        img : "assets/images/pieces/white/rook.png",
+export function whiteQueen(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/white/queen.png",
+        piece_name : "WHITE_QUEEN",
     };
 }
 
-export function whiteRook(cuurent_position){
-    return {
-        cuurent_position,
-        img : "assets/images/pieces/white/rook.png",
+export function whiteKing(current_position){
+    return{
+        current_position,
+        img : "assets/images/pieces/white/king.png",
+        piece_name : "WHITE_KING",
     };
 }
 
